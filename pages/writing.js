981c24d0311999d9a706c6e680675aa71677ad21
Steve Barbaro <steve.barbaro@gmail.com>
2022-03-17 20:08:03 -0500

    gets yet deeper into writing page links

--- pages/writing.js
+++ pages/writing.js
@@ -11,34 +11,78 @@ export default function Writing() {
       <p>––<span className={styles.grayTextUppercase}>online</span>––</p>
       <br/>
         <p>[<span className={styles.grayText}>fiction</span>]<br/><br/>
-          “Celestial Camo” – <a target="_blank" href="https://socratesonthebeach.com/steve-barbaro">Socrates on The Beach</a> – ( .. in Sicily .. a tale of inebriated reckoning .. )
+          “<span className={styles.pieceTitleText}>Celestial Camo</span>” – <a target="_blank" href="https://socratesonthebeach.com/steve-barbaro">Socrates on The Beach</a> – ( .. in Sicily .. a tale of inebriated reckoning .. )
         </p>
         <p>
-        "A Genealogy of Instinct" – <a target="_blank" href="http://www.conjunctions.com/online/article/steve-barbaro-09-17-2013">Conjunctions</a> – ( .. also in Sicily .. porcupines .. ancestry .. the blurry migration of dreams .. )
+        "<span className={styles.pieceTitleText}>A Genealogy of Instinct</span>" – <a target="_blank" href="http://www.conjunctions.com/online/article/steve-barbaro-09-17-2013">Conjunctions</a> – ( .. also in Sicily .. porcupines .. ancestry .. the blurry migration of dreams .. )
         </p>
         <p>
-        “Um…A Sensorium” – <a target="_blank" href="http://www.3ammagazine.com/3am/um-a-sensorium/">3:AM</a> – ( .. in a room overlooking a street .. synaesthesia + biological odysseys .. )
+        “<span className={styles.pieceTitleText}>Um…A Sensorium</span>” – <a target="_blank" href="http://www.3ammagazine.com/3am/um-a-sensorium/">3:AM</a> – ( .. in a room overlooking a street .. synaesthesia + biological odysseys .. )
         </p><br/><br/>
           <p>[<span className={styles.grayText}>poetry</span>]<br/><br/>
-          "For Georges Braque" – <a target="_blank" href="http://thediagram.com/16_1/barbaro.html">DIAGRAM</a> – ( .. utensils .. the body .. geometry .. )
+          "<span className={styles.pieceTitleText}>For Georges Braque</span>" – <a target="_blank" href="http://thediagram.com/16_1/barbaro.html">DIAGRAM</a> – ( .. utensils .. the body .. geometry .. )
           </p>
           <p>
-          "Quadratic" – <a target="_blank" href="https://yalereview.org/article/quadratic">The Yale Review</a> – ( .. property .. suppression .. the otherworldly .. )
+          "<span className={styles.pieceTitleText}>Quadratic</span>" – <a target="_blank" href="https://yalereview.org/article/quadratic">The Yale Review</a> – ( .. property .. suppression .. the otherworldly .. )
           </p>
           <p>
-          "Sorta" + "For a Friend" + "The Lighthouse Verdict" – <a target="_blank" href="http://www.conjunctions.com/online/article/steve-barbaro-07-17-2012">Conjunctions</a> – ( .. estrangement .. crowding .. whatever's jangly .. )
+          "<span className={styles.pieceTitleText}>Sorta</span>" + "<span
+          className={styles.pieceTitleText}>For a Friend</span>" + "<span
+          className={styles.pieceTitleText}>The Lighthouse Verdict</span>" – <a target="_blank" href="http://www.conjunctions.com/online/article/steve-barbaro-07-17-2012">Conjunctions</a> – ( .. estrangement .. crowding .. )
           </p>
           <p>
-          "Graffito Beholds a Sculpted Dionysus Head" – <a target="_blank" href="https://www.thecommononline.org/graffito-beholds-a-sculpted-doinysus-head">The Common</a> – ( .. self .. explanatory .. )
+          "<span className={styles.pieceTitleText}>Graffito Beholds a Sculpted Dionysus Head</span>" – <a target="_blank" href="https://www.thecommononline.org/graffito-beholds-a-sculpted-doinysus-head">The Common</a> – ( .. self .. explanatory .. )
           </p>
           <p>
-          "Wee" – <a target="_blank" href="https://preludemag.com/posts/wee/">Prelude</a> – ( .. hmmmmmmmmm .. OK .. )
+          "<span className={styles.pieceTitleText}>Wee</span>" – <a target="_blank" href="https://preludemag.com/posts/wee/">Prelude</a> – ( .. hmmmmmmmmm .. OK .. )
           </p>
           <p>
-          “The Bottle of Schnapps” – <a target="_blank" href="https://www.therupturemag.com/rupture/the-bottle-of-schnapps">The Rupture</a> – ( .. memory .. aphrodisiacs .. forgetting )
+          “<span className={styles.pieceTitleText}>The Bottle of Schnapps</span>” – <a target="_blank" href="https://www.therupturemag.com/rupture/the-bottle-of-schnapps">The Rupture</a> – ( .. memory .. aphrodisiacs .. forgetting .. )
           </p>
           <p>
-          "In The Omnibus (1891)" – <a target="_blank" href="http://thecollagist.com/the-collagist/2018/1/20/in-the-omnibus-1891.html">The Collagist</a> – ( .. ekphrasis .. Cassatt .. functionality )
+          “<span className={styles.pieceTitleText}>Ontbijtjes</span>” – <a target="_blank" href="https://www.natbrut.com/steve-barbaro">Nat. Brut</a> – ( .. coffee .. flooding .. dillydallyings .. )
+          </p>
+          <p>
+          "<span className={styles.pieceTitleText}>In The Omnibus (1891)</span>" – <a target="_blank" href="http://thecollagist.com/the-collagist/2018/1/20/in-the-omnibus-1891.html">The Collagist</a> – ( .. Mary .. Cassatt .. functionality .. )
+          </p>
+          <p>
+          "<span className={styles.pieceTitleText}>Summons in the Form of an Evasion</span>" + "<span
+          className={styles.pieceTitleText}>37 Panoramic Views of Edo</span>" – <a target="_blank" href="https://interim.squarespace.com/barbaro">Interim</a> – ( .. volcanism .. flapping .. )
+          </p>
+          <p>
+          "<span className={styles.pieceTitleText}>The Pond</span>" + "<span
+          className={styles.pieceTitleText}>Articles of Capitulation</span>" – <a target="_blank" href="https://positjournal.com/2018/03/12/steve-barbaro/">Posit</a> – ( .. Cézanne .. fumaroles .. Caillebotte .. )
+          </p>
+          <p>
+          "<span className={styles.pieceTitleText}>Of Property + Precipitation</span>" – <a target="_blank" href="https://preludemag.com/posts/of-property-precipitation/">Prelude</a> – ( .. intrusion .. skulls .. lackeys .. )
+          </p>
+          <p>
+          "<span className={styles.pieceTitleText}>Disk</span>" + "<span
+          className={styles.pieceTitleText}>Pare</span>" + "<span
+          className={styles.pieceTitleText}>What A Perky Little Edifice</span>" + "<span
+          className={styles.pieceTitleText}>Letter</span>" + "<span
+          className={styles.pieceTitleText}>Post-Archipelago</span>" + "<span
+          className={styles.pieceTitleText}>Tychtrip</span>" – <a target="_blank" href="https://theelephants.net/records/steve-barbaro">The Elephants</a> – ( .. the gamut .. everything .. nothing .. )
+          </p>
+          <p>
+          “<span className={styles.pieceTitleText}>The Surface Appliance</span>” – <a target="_blank"
+          href="http://thediagram.com/13_1/barbaro.html">DIAGRAM</a> – ( .. icecastles .. weaponry .. )
+          </p>
+          <p>
+          “<span className={styles.pieceTitleText}>Concave Decades</span>” – <a target="_blank"
+          href="https://yalereview.org/article/concave-decades">The Yale Review</a> – ( .. plates .. alienation .. plates .. )
+          </p>
+          <p>
+          “<span className={styles.pieceTitleText}>Space from Nothing</span>” – <a target="_blank"
+          href="http://www.versedaily.org/2013/spacefromnothing.shtml">Verse Daily</a> – (via <i>The Literary Review</i>) ( .. old tombs .. airplanes .. )
+          </p>
+          <p>
+          “<span className={styles.pieceTitleText}>Rue'd</span>” – <a target="_blank"
+          href="https://preludemag.com/posts/rued/">Prelude</a> – ( .. well .. ahem .. see .. )
+          </p>
+          <p>
+          "<span className={styles.pieceTitleText}>Space + Gesture</span>" + "<span
+          className={styles.pieceTitleText}>Portrait of Henri Rochefort (1881)</span>" – <a target="_blank" href="http://thediagram.com/9_6/barbaro.html">DIAGRAM</a> – ( .. rain .. globes .. cheeks .. )
           </p>
 
       </div>
