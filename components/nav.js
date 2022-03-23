9d3067e060d8ddfd5bc4423cd6b84eacda1e2ffb
Steve Barbaro <steve.barbaro@gmail.com>
2022-03-23 11:15:48 -0500

    refines sundry styles

--- components/nav.js
+++ components/nav.js
@@ -5,7 +5,9 @@ export default function Nav() {
   return (
 
 <div className={styles.nav}>
-<div className={styles.name}>Steve Barbaro</div>
+<div className={styles.name}>St<span className={styles.alternateLettering}>e</span>ve B<span
+className={styles.alternateLettering}>a</span>r<span className={styles.alternateLettering}>b</span>ar<span
+className={styles.alternateLettering}>o</span></div>
 <div className={styles.buttonBox}>
 <Link href="/" passHref><button className={styles.navButton}>home</button></Link>
 <Link href="/writing" passHref><button className={styles.navButton}>writing</button></Link>
