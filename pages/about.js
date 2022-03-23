e2e7e38a312aec386aa48572210551923ffe3e3e
Steve Barbaro <steve.barbaro@gmail.com>
2022-03-23 16:06:39 -0500

    refines home page font colors

--- pages/about.js
+++ pages/about.js
@@ -24,8 +24,8 @@ export default function About() {
       Conjunctions</i> + <i>DIAGRAM</i> + <i>Denver Quarterly</i>. A
       former Poe-Faulkner Fellow at the University of Virginia where he earned his MFA, Steve is also
       the founder + editor + programmer + designer of <a target="_blank" href="https://newnewsinews.com">new sinews</a></p><br/><br/><br/>
-      ( ...all of the photographs on this website were taken by
-      Steve + all of this website was likewise programmed + designed by Steve... )
+      ( ... all of the photographs on this website were taken by
+      Steve ... + all of this website was likewise programmed + designed by Steve ... )
       </div>
       </div>
    </>
